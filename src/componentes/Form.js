0d55668aac992d21b883f3ef889e3435065415da
micaaranguiz <micaelaaranguiz2@gmail.com>
2023-05-04 14:12:38 -0300

    spa

--- src/componentes/Form.js
+++ src/componentes/Form.js
@@ -17,29 +17,6 @@ function Form() {
                             <input type="password" class="form-control" id="inputPassword3"></input>
                         </div>
                     </div>
-                    <fieldset class="row mb-3">
-                        <legend class="col-form-label col-sm-2 pt-0">Radios</legend>
-                        <div class="col-sm-10">
-                            <div class="form-check">
-                                <input class="form-check-input" type="radio" name="gridRadios" id="gridRadios1" value="option1" checked>
-                                </input> <label class="form-check-label" for="gridRadios1">
-                                        First radio
-                                    </label>
-                            </div>
-                            <div class="form-check">
-                                <input class="form-check-input" type="radio" name="gridRadios" id="gridRadios2" value="option2">
-                                </input>  <label class="form-check-label" for="gridRadios2">
-                                        Second radio
-                                    </label>
-                            </div>
-                            <div class="form-check disabled">
-                                <input class="form-check-input" type="radio" name="gridRadios" id="gridRadios3" value="option3" disabled>
-                                </input> <label class="form-check-label" for="gridRadios3">
-                                        Third disabled radio
-                                    </label>
-                            </div>
-                        </div>
-                    </fieldset>
                     <div class="row mb-3">
                         <div class="col-sm-10 offset-sm-2">
                             <div class="form-check">
--- src/pages/Productos.js
+++ src/pages/Productos.js
@@ -1,6 +1,12 @@
 import React from 'react';
 import Card from '../componentes/Card';
 import card1 from '../card1.jpg'
+import card2 from '../card2.jpg'
+import card3 from '../card3.png'
+import card4 from '../card4.jpg'
+import card5 from '../card5.jpg'
+
+
 import './Productos.css';
 
 function Productos() {
@@ -8,10 +14,10 @@ function Productos() {
         <>
             <div class="disposicion">
                 <Card img={card1} titulo="Temporada Otoño-Inivierno" descripcion="Las mejores zapatillas de la temporada" />
-                <Card img={card1} titulo="Temporada Otoño-Inivierno" descripcion="Las mejores zapatillas de la temporada" />
-                <Card img={card1} titulo="Temporada Otoño-Inivierno" descripcion="Las mejores zapatillas de la temporada" />
-                <Card img={card1} titulo="Temporada Otoño-Inivierno" descripcion="Las mejores zapatillas de la temporada" />
-                <Card img={card1} titulo="Temporada Otoño-Inivierno" descripcion="Las mejores zapatillas de la temporada" />
+                <Card img={card2} titulo="Temporada Otoño-Inivierno" descripcion="Las mejores zapatillas de la temporada" />
+                <Card img={card3} titulo="Temporada Otoño-Inivierno" descripcion="Las mejores zapatillas de la temporada" />
+                <Card img={card4} titulo="Temporada Otoño-Inivierno" descripcion="Las mejores zapatillas de la temporada" />
+                <Card img={card5} titulo="Temporada Otoño-Inivierno" descripcion="Las mejores zapatillas de la temporada" />
             </div>
             <div class="disposicion">
                 <Card img={card1} titulo="Temporada Otoño-Inivierno" descripcion="Las mejores zapatillas de la temporada" />
